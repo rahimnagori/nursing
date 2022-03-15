64277ac7b18c7fc1fe6a03d9b895995f6fd69688
rahim <rahim.nagori@gmail.com>
2022-03-16 01:11:30 +0530

    session library added

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,6 +6,7 @@ class Admin extends CI_Controller {
   public function __construct() {
     parent::__construct();
     $this->load->model('Common_Model');
+    $this->load->library('session');
     // $this->check_login();
   }
 
