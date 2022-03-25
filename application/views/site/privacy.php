d79130a30aaffe7186607729ed3e4c2f783dfb56
rahim <rahim.nagori@gmail.com>
2022-03-25 22:51:19 +0530

    Privacy policy updated

--- application/views/site/privacy.php
+++ application/views/site/privacy.php
@@ -1,33 +1,130 @@
 <div class="inner_cont">
-      <div class="container">
-         <h4>Privacy Policy </h4>
-      <p>
-         <span><a href="<?=site_url();?>">Home</a></span>
-         <span>Privacy Policy </span>
-      </p>
-      </div>
-   </div>
+  <div class="container">
+    <h4>Privacy Policy </h4>
+    <p>
+      <span><a href="<?= site_url(); ?>">Home</a></span>
+      <span>Privacy Policy </span>
+    </p>
+  </div>
+</div>
 <div class="abbout_us pad_sec ">
   <div class="container">
-      
+    <h3>About this website</h3>
+    <p>This is pambron.comwebsite. The pambron.com website is owned and operated by Pambron Nursing Agency Limited, a Company incorporated under the Laws of England and Wales with Company Registration Number 12015542,carrying on business as a Nursing Agency in all parts of the United Kingdom, and having its Registered Office at Unit 7, Daisy Business Park, 19-35 Sylvan Grove, London SE15 1PD. We are committed to protecting and preserving the privacy of the users of our website and visitors to our websites. We are also committed to protecting the privacy of users or visitors communicating electronically with us.</p>
 
+    <p>This Privacy Policy sets out how we process, and use any personal data you provide to us or that we collect from you through your use or visit to our website. We can confirm that we will keep your information secure and that we will comply fully with all applicable UK Data Protection legislation and regulations. This Privacy Policy has been drafted in accordance with the Laws that operate in the United Kingdom of Great Britain and Northern Ireland.</p>
 
-      <h4>Why do we use it?</h4>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-      <p>
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-      </p>
-      <h4>Where does it come from?</h4>
-      <ul>
-            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-            <li>Sed a tortor nec turpis aliquet vehicula non quis nisl.</li>
-            <li>Aenean non nisl ullamcorper purus placerat faucibus a sit amet dolor.</li>
-            <li>Vivamus aliquam felis eget lorem malesuada mollis.</li>
-            <li>Curabitur viverra mauris a nulla elementum, ac commodo leo egestas.</li>
-        </ul>
-        <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <p>
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
+    <h3>Making Change to this Policy.</h3>
+    <p>We reserve the right to amend, modify or substitute this privacy policy from time to time. When a change or amendment is made to this policy, it will be made on the body of this policy (amended version) and the amended versions of the privacy policy will be posted on our website. Where the privacy policy is replaced altogether, the new policy will be posted on our website and this privacy policy will be removed.</p>
+
+    <h3>How we Collect Information</h3>
+    <p>We collect information in two ways:</p>
+    <ol>
+      <li><strong>Automatically: </strong>We may collect information automatically using cookies.</li>
+      <li><strong>Provided by you: </strong>We may collect information through details directly provided by you during registration, processing of your application, acceptances of offer of placement or in the normal use of our services/website.</li>
+    </ol>
+
+    <h3>Information we may request/collect from you</h3>
+    <p>Every information about you that we may require you to enter at the time of registering with us, or while processing your application. We may request any information that we consider necessary to carrying out the services that you require. This required information may change from time to time. Information we may request, could include, but not limited to:</p>
+    <ul>
+      <li>Your name</li>
+      <li>Your postal address</li>
+      <li>Your telephone numbers</li>
+      <li>Your email address</li>
+      <li>current and previous employment history</li>
+      <li>Education, qualifications, and trainings attended</li>
+      <li>Records of Criminal Convictions</li>
+    </ul>
+
+    <h3>Information we may request/collect from you</h3>
+    <p>Every information about you that we may require you to enter at the time of registering with us, or while processing your application. We may request any information that we consider necessary to carrying out the services that you require. This required information may change from time to time. Information we may request, could include, but not limited to:</p>
+    <ul>
+      <li>Your name</li>
+      <li>Your postal address</li>
+      <li>Your telephone numbers</li>
+      <li>Your email address</li>
+      <li>current and previous employment history</li>
+      <li>Education, qualifications, and trainings attended</li>
+      <li>Records of Criminal Convictions</li>
+    </ul>
+
+    <h3>How we use information Collected</h3>
+    <ol>
+      <li>For the administration of Services that we provide to you, and our clients and to help in the proper management of any account that you hold with us; or services that we offer to you. </li>
+      <li>To identify and properly respond to you when you make any request from us.</li>
+      <li>Detection and Prevention of fraud.</li>
+      <li>Where you have provided us with your consent to do so, to contact you regarding other products/vacancies, and services offered by this website, and any other products and services offered by the company operating this website.</li>
+    </ol>
+
+    <h3>With whom we might share your information</h3>
+    <p>There are circumstances in which we may share your information or receive information about you:
+    </p>
+    <ol>
+      <li>
+        Staff or other members of our company that require such information while carrying out their functions as staff members of our company.
+      </li>
+      <li>
+        Client’s that may require such information in the course normal course of seeking to offer a place to an applicant.
+      </li>
+      <li>
+        Third Party Rights, Enforcement of Public Policies, Company Rights and Legal Disclosure. We may disclose your personal information if required by law or to comply with an order of court order or when we believe that disclosure is necessary to protect our rights, required for the protection of your safety or the safety of others; respond to a government request and/or investigate fraud.
+      </li>
+      <li>
+        Internet service providers and third-party business partners. We may supply your Personal Data to our internet service providers and third-party business partners. This is because it may be necessary to do this for the proper administration of our website. It is also important to kindly note that some of these third parties may sometimes be located outside of the United Kingdom; but we ensure through a binding agreement that they provide privacy policies that are comparable to what is required of us by the Law of the Country that we operate. We also make these third parties sign a non-disclosure agreement with us.
+      </li>
+      <li>
+        Third parties that we may use to collect feedback and reviews of our services from you.
+      </li>
+    </ol>
+
+    <h3>Sending your information outside the EEA</h3>
+    <p>There are situations when some of our third-party partners may be outside the EEA, We will only send your personal information to providers outside the EEA if those providers are required to protect personal information in the same way they would be required to protect personal information where they were within the EEA. We will send data to such recipient only if:
+    </p>
+    <ol>
+      <li>The Third-Party recipient has by a written agreement with us, agreed to comply with the requirements for standards of personal information processing and protection set under the appropriate EEA Laws/Regulations.</li>
+      <li>Where the European Commission has determined that the non-EEA country engages in an adequate and appropriate level of protection for personal data.</li>
+      <li>Data may be sent to the US in accordance with the relevant protection shield.</li>
+    </ol>
+
+    <h3>Communications we might send to you</h3>
+    <p>There may be circumstances where we need to send you some communications. Unless where extremely necessary, we limit our communication with you, strictly to Service communications. These service communications comprise of communications which:
+    </p>
+    <ol>
+      <li>We send to you when you require us to carry out a service. These are services which you initiate. These services may include password reset emails, which you may ask us to send if you do not remember your login credentials but want to access our products and services.</li>
+      <li>We send immediately after you have registered to access our services; or when you cancel your subscription with us.</li>
+      <li>Available vacancies or staff matching your profile/previous requests.</li>
+      <li>We may send information to notify of payments, acknowledge receipt of payment or bring the existence of an invoice to your attention. </li>
+    </ol>
+
+    <h3>Cookies</h3>
+    <p>When you use our website, our website is designed to send cookies to your computer. Cookies is a piece of data sent from our website to your computer when you visit our site; and this information is stored on your computer by the user’s web browser whilst you are browsing. Cookies help make this website work and provide information to us about how users interact with our site. We use this information to improve our website. It helps our website to remember you when you come back to the website.</p>
+
+    <h3>Third Party Websites Cookies</h3>
+    <p>There are other times when third party websites cookies may be sent to your website if you access a third-party website through our website or where there are links to such websites from our website. In such cases, your information is governed by their privacy policies, and you may use the privacy policies of such third-party websites to opt-out from such information collection.</p>
+
+    <h3>How long we retain your Data</h3>
+    <p>Data automatically collected by us, or provided by you are retained for as long as your account remains active or as required to provide services to you, but in accordance with the laws guiding retention of data in the United Kingdom. Data held on you may also be deleted when you decide to opt-out or require that they be deleted. Once we deem any data held as no longer necessary for relevant uses, we may at our discretion, delete or amend any such data held on our systems.</p>
+
+    <h3>Right of access to your data</h3>
+    <p>You can exercise your “Subject Access Right” to ensure that your personal data is being processed lawfully. Your subject access right can be exercised in accordance with data protection laws and regulations. Any subject access request must be made in writing to the Data controller either by email (contact us link below) or by post at the address provided in the contact section of this privacy policy.</p>
+
+    <h3>Responsibility for Policy</h3>
+    <p>Responsibility for this policy, including an annual documented review, is that of our Data Controller. Requests regarding all data collected or data retained pursuant to this privacy policy, including but not limited to the Data Controller.</p>
+
+    <h3>Contacting us</h3>
+    <p>If you have any enquiries/concern relating to information held by us, please contact us,:datacontroller@pambron.com</p>
+    <p>or write to:
+      Data Controller
+      <br />
+      Pambron Nursing Agency Limited
+      <br />
+      Unit 7 Daisy Business Park
+      <br />
+      19-35 Sylvan Grove
+      <br />
+      London
+      <br />
+      SE15 1PD
+    </p>
   </div>
 </div>
